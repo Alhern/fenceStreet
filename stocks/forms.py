69ed7a0e0ae88193ac1b78edea1ea2b393fc28b8
SK-117 <alliotonio@gmail.co>
2021-06-16 02:56:02 +0200

    ask conf for psw creation

--- stocks/forms.py
+++ stocks/forms.py
@@ -9,9 +9,19 @@ class StockForm(forms.ModelForm):
 
 
 class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(widget=forms.PasswordInput())
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('username', 'email', 'password')
+
+    #Confirmation du mot de passe
+    def clean(self):
+        cleaned_data = super(RegisterForm, self).clean()
+        password = cleaned_data.get("password")
+        password_confirmation = cleaned_data.get("password_confirmation")
+        if password != password_confirmation:
+            self.add_error('password', "Passwords are not matching")
 
 
 class LoginForm(forms.Form):
